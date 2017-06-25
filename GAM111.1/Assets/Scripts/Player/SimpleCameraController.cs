d45cef90215fb6061dcda27653d0fb45530e837e
Nathan Diill - Laptop <1004946@student.sae.edu.au>
2017-06-26 00:42:47 +1000

    Player added. Player controller added. Basic camera controller added. Textures added.

---
+++ GAM111.1/Assets/Scripts/Player/SimpleCameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleCameraController : MonoBehaviour 
+{
+    public GameObject Player;
+    public Vector3 Offset;
+
+	void Start ()
+    {
+        // Set the offset to the player position
+        Offset = transform.position - Player.transform.position;	
+	}
+	
+	void LateUpdate () 
+    {
+        // Add the offset
+        transform.position = Player.transform.position + Offset;
+    }
+}
---
+++ GAM111.1/Assets/Scripts/Player/SimplePlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimplePlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float torqueScale = 1;
+    public Vector3 previousTorque;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () 
+    {
+        var inputRot = Camera.main.transform.rotation;
+        var tmp = inputRot.eulerAngles;
+        tmp.x = 0;
+        tmp.z = 0;
+        inputRot = Quaternion.Euler(tmp);
+
+        var torqueDir = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal")).normalized;
+        torqueDir = inputRot * torqueDir;
+
+        previousTorque = torqueDir * torqueScale;
+
+        rb.AddTorque(previousTorque);
+
+        Debug.DrawRay(transform.position, previousTorque, Color.red);
+	}
+}
