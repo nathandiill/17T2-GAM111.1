2d4c366a82dfa42da6ab87ce699ae18c5cff24f5
Nathan Diill - Laptop <1004946@student.sae.edu.au>
2017-06-27 10:13:27 +1000

    Bouncy Wall added in.

---
+++ GAM111.1/Assets/Scripts/Game/Bounciness.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounciness : MonoBehaviour {
+
+    public float thrust = 125;
+    public Rigidbody rb;
+    public Vector3 ForceDirection;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+	void OnCollisionEnter (Collision col)
+    {
+        Vector3 Forcetoapply = transform.TransformDirection(ForceDirection);
+        col.rigidbody.AddForce(Forcetoapply * thrust, ForceMode.Impulse);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 Forcetoapply = transform.TransformDirection(ForceDirection);
+        Gizmos.DrawRay(transform.position, Forcetoapply);
+    }
+}
