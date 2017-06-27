448f6f704f715738d6ee55d999686d4bd4cd608b
Nathan Diill - Laptop <1004946@student.sae.edu.au>
2017-06-27 11:39:09 +1000

    Bounce pad done. Camera is smart now.

---
+++ GAM111.1/Assets/Scripts/Player/CameraTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour
+{
+    public SimpleCameraController.CameraMode modeToSetTo;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Camera.main.GetComponent<SimpleCameraController>().cameraMode = modeToSetTo;
+        }
+    }
+}
--- GAM111.1/Assets/Scripts/Player/SimpleCameraController.cs
+++ GAM111.1/Assets/Scripts/Player/SimpleCameraController.cs
@@ -5,16 +5,37 @@ public class SimpleCameraController : MonoBehaviour
 {
     public GameObject Player;
     public Vector3 Offset;
+    public Vector3 FallOffset;
+    //public Vector3 CameraRotate;
+    public CameraMode cameraMode;
+    public Vector3 OffsetAngles;
+    public Vector3 FallingAngles;
+
+    public enum CameraMode
+    {
+        Default,
+        Falling
+    }
 
 	void Start ()
     {
         // Set the offset to the player position
-        Offset = transform.position - Player.transform.position;	
+        // Offset = transform.position - Player.transform.position;	
 	}
-	
-	void LateUpdate () 
+
+    void LateUpdate () 
     {
-        // Add the offset
-        transform.position = Player.transform.position + Offset;
+        if (cameraMode == CameraMode.Falling)
+        {
+            transform.position = Player.transform.position + FallOffset;
+            transform.rotation = Quaternion.Euler(FallingAngles);
+        }
+        else
+        {
+            transform.position = Player.transform.position + Offset;
+            transform.rotation = Quaternion.Euler(OffsetAngles);
+        }
+
+        
     }
 }
