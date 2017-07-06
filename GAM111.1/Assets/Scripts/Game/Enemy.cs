fa1274a0bf124a6559ab04fb01ccb6583ab5967a
NATMAN-LAPTOP <1004946@student.sae.edu.au>
2017-07-07 06:43:53 +1000

    Particle system working. Just need to do audio.

--- GAM111.1/Assets/Scripts/Game/Enemy.cs
+++ GAM111.1/Assets/Scripts/Game/Enemy.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
 
     public GameObject enemy;
+    public Transform prefab;
 
-	public void OnCollisionEnter(Collision col)
+    public void OnCollisionEnter(Collision col)
     {
         // If enemy hits the player, destroy self and take away points
         if(GameObject.FindGameObjectWithTag("Player"))
         {
             PointsSystem.Instance.points -= 100;
+            Instantiate(prefab, enemy.transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
